f37f2496444ac82f9125be80d616cf2530d6a966
Thomas Handschuh <thomas.handschuh@rise-world.com>
2021-03-05 14:06:32 +0100

    removes test target
    swifter dependency prevents usage of OAuthSwift https://github.com/httpswift/swifter/issues/452

--- Package.swift
+++ Package.swift
@@ -28,12 +28,8 @@ let package = Package(
         .library(name: "OAuthSwift", targets: ["OAuthSwift"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/phimage/Erik.git", .upToNextMajor(from: "5.0.0")),
-        .package(url: "https://github.com/tid-kijyun/Kanna.git", .upToNextMajor(from: "5.0.0")),
-        .package(url: "https://github.com/httpswift/swifter.git", .upToNextMajor(from: "1.4.7"))
     ],
     targets: [
         .target(name: "OAuthSwift", dependencies: [], path: "Sources"),
-        .testTarget(name: "OAuthSwiftTests", dependencies: ["OAuthSwift", "Erik", "Kanna", "Swifter"], path: "OAuthSwiftTests"),
     ]
 )
